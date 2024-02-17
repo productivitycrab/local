44d3d6955c815dcb34accd6fe7c8945d8ebf6dd3
loodle <lee@03172017.com>
2024-02-17 11:33:24 -0700

    initialized

---
+++ angel-wt/server/angelapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.timezone import now
+
+class Reminder(models.Model):
+    SIT_STAND = 'sit_stand'
+    FOOD = 'food'
+    MEDICATION = 'medication'
+    TOOTHBRUSH = 'toothbrush'
+    UNCATEGORIZED = 'uncategorized'
+
+    TYPE_CHOICES = [
+        (SIT_STAND, 'Sit/Stand'),
+        (FOOD, 'Food'),
+        (MEDICATION, 'Medication'),
+        (TOOTHBRUSH, 'Toothbrush'),
+        (UNCATEGORIZED, 'Uncategorized'),
+    ]
+    
+    reminder_type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=UNCATEGORIZED
+    )
+
+    reminder_date = models.DateField(null=True)
